b560f0442e03171594154799a8288b6231bd7954
Facundo <facundodenis7@gmail.com>
2024-03-05 18:04:12 -0300

    cambios_17_devep

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pandas as pd
 import numpy as np
 import pyarrow as pa
 import pyarrow.parquet as pq
-from fastapi import FastAPI
-from sklearn.metrics.pairwise import cosine_similarity
 from fastapi import FastAPI, HTTPException, Response
-from fastapi.responses import HTMLResponse
-from fastapi.responses import JSONResponse
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 app = FastAPI()
 
@@ -22,52 +20,15 @@ df_UsersWorstDeveloper = pd.read_parquet("data/UsersWorstDeveloper.parquet")
 df_Sentiment_Analysis = pd.read_parquet("data/sentiment_analysis.parquet")
 
 
-@app.get(path="/", response_class=HTMLResponse, tags=["Home"])
-@app.get("/presentacion")
-def presentacion():
-    '''
-    Genera una página de presentación HTML para la API Steam de consultas de videojuegos.
-    
-    Returns:
-    str: Código HTML que muestra la página de presentación.
-    '''
-    return '''
-    <html>
-        <head>
-            <title>API Steam</title>
-            <style>
-                body {
-                    font-family: Arial, sans-serif;
-                    padding: 20px;
-                }
-                h1 {
-                    color: #333;
-                    text-align: center;
-                }
-                p {
-                    color: #666;
-                    text-align: center;
-                    font-size: 18px;
-                    margin-top: 20px;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>API de consultas de videojuegos de la plataforma Steam</h1>
-            <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
-            <p>INSTRUCCIONES:</p>
-            <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
-            <p> Visita mi perfil en <a href="https://www.linkedin.com/in/facundo-nicolas-denis-60933b199/"><img alt="LinkedIn" src="https://img.shields.io/badge/LinkedIn-blue?style=flat-square&logo=linkedin"></a></p>
-            <p> El desarrollo de este proyecto esta en <a href="https://github.com/Facundo022/PI1_MLOps_videojuegos_steam"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
-        </body>
-    </html>
-    '''
-
 
 ### correccion, funciones 0.1
 
 @app.get("/Developer")
-def developer(nombre_desarrollador):
+def Developer(nombre_desarrollador):
+    """
+    ingresa numbre de desarrollador
+    devuelve: Cantidad de items y porcentaje de contenido Free según empresa desarrolladora
+    """
     try:
         # Filtrar el DataFrame por el nombre del desarrollador
         desarrollador_filtrado = df_developer[df_developer["developer"] == nombre_desarrollador]
@@ -86,7 +47,7 @@ def developer(nombre_desarrollador):
             "Cantidad de juegos desarrollados: " : cantidad_juegos,
             "Porcentaje de ellos free: ": porcentaje_free
         }
-        return JSONResponse(content = resultado)
+        return resultado
     except Exception as e:
         return {"error": str(e)}
 
